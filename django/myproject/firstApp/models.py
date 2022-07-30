8cb979869be93d8366db5d8598f2b93c1cbc596c
morgan <morgan@2806-10ae-0019-b3f6-0000-0000-0000-0001.ipv6.infinitum.net.mx>
2022-07-29 21:13:06 -0500

    django proyect deleted

--- django/myproject/firstApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class item (models.Model):
-
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
-    #available: bool
