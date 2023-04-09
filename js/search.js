99dd8b6b66a34766f7f8d10b2cbf45d7a9485e14
AlanIMorgan <alan.morgana@alumno.buap.mx>
2023-04-09 14:42:28 -0600

    Bug fixes

--- js/search.js
+++ js/search.js
@@ -262,9 +262,11 @@ function openTab(google, array) {
 
 	directAccess.style.display = "block";
 
+	txt = array.replace("!", "%21").replace('"', "%22").replace("#", "%23").replace("$", "%24").replace("%", "%25").replace("&", "%26").replace("'", "%27").replace("(", "%28").replace(")", "%29").replace("*", "%2A").replace("+", "%2B").replace(",", "%2C").replace("/", "%2F").replace(":", "%3A").replace(";", "%3B").replace("=", "%3D").replace("?", "%3F").replace("@", "%40").replace("[", "%5B").replace("]", "%5D");
+
 	setTimeout(() => {
 
-		window.open(query + array);
+		window.open(query + txt);
 	}, 250);
 
 	updateHistory();
